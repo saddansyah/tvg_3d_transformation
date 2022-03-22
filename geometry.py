update - fix some issues on rotation func

geometry.py
@@ -42,7 +42,7 @@ class Geometry:
         roteted_geometry = np.empty((0,3))
 
         for point in self.__geometry:
-            roteted_geometry = np.append(roteted_geometry, Transform.rotation(point, (rx, ry, rz), r_type).reshape((1,3)), axis=0)
+            roteted_geometry = np.append(roteted_geometry, Transform.rotation(point, (-rx, -ry, -rz), r_type).reshape((1,3)), axis=0)
         
         self.__geometry = roteted_geometry        
 

transform.py
@@ -14,7 +14,7 @@ class Transform:
         x = point3D[0]
         y = point3D[1]
         z = point3D[2]
-        angle = math.pi/6
+        angle = math.pi/4
 
         projection_matrix = np.array([[1, 0, 0.5*math.cos(angle)],
                                       [0, 1, -0.5*math.sin(angle)],