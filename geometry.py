fixing issues on rotation()

geometry.py
@@ -7,7 +7,7 @@ from abc import ABC, abstractmethod
 class Geometry:
         
     def __init__(self, geometry: np.ndarray, screen_width, screen_height):
-        self.__scaling = 75
+        self.__scaling = 80
         self.__geometry = geometry * self.__scaling
 
     def get_coord(self):

transform.py
@@ -3,6 +3,9 @@ import math
 
 
 class Transform:
+
+    __scaling = 0
+
     def __init__(self):
         pass
 
@@ -28,6 +31,8 @@ class Transform:
 
     @staticmethod
     def translation(point: np.ndarray, tx, ty, tz, scaling):
+        __scaling = scaling
+
         if(len(point) < 4):
             point = np.append(point, 1)
 
@@ -140,7 +145,7 @@ class Transform:
         if(len(point) < 4):
             point = np.append(point, 1)
 
-        point = Transform.translation(point, -r_axis[0], -r_axis[1], -r_axis[2])
+        point = Transform.translation(point, -r_axis[0], -r_axis[1], -r_axis[2], cls.__scaling)
 
         if r_type == "x":
             point = cls.rotationx(point, r_factor[0])
@@ -149,4 +154,4 @@ class Transform:
         elif r_type == "z":
             point =  cls.rotationz(point, r_factor[2])
 
-        return Transform.translation(point, r_axis[0], r_axis[1], r_axis[2])
+        return Transform.translation(point, r_axis[0], r_axis[1], r_axis[2], cls.__scaling)